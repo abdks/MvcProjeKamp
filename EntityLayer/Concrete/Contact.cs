022209ad3406b5757456fedd9e54646dbc716319
AbdullahKus <62968246+abdks@users.noreply.github.com>
2023-10-23 20:28:28 +0300

    Update v3

---
+++ EntityLayer/Concrete/Contact.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Contact
+    {
+        [Key]
+        public int ContactID { get; set; }
+        [StringLength(100)]
+
+        public string Username { get; set; }
+        [StringLength(100)]
+
+        public string UserMail { get; set; }
+        [StringLength(300)]
+
+        public string Subject { get; set; }
+        [StringLength(2500)]
+
+        public string Message { get; set; }
+        
+
+    }
+}
