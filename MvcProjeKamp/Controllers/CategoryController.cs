88fe1cc592732e450ef9af848a7e540b21b3de1f
AbdullahKus <62968246+abdks@users.noreply.github.com>
2023-10-26 20:54:42 +0300

    Update 4.0

--- MvcProjeKamp/Controllers/CategoryController.cs
+++ MvcProjeKamp/Controllers/CategoryController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,8 +33,22 @@ namespace MvcProjeKamp.Controllers
         [HttpPost]
         public ActionResult AddCategory(Category p)
         {
-           // cm.CategoryAddBL(p);
-            return RedirectToAction("GetCategoryList");
+            // cm.CategoryAddBL(p);
+            CategoryValidatior categoryValidatior = new CategoryValidatior();
+            ValidationResult result = categoryValidatior.Validate(p);
+            if(result.IsValid)
+            {
+                cm.CategoryAdd(p);
+                return RedirectToAction("GetCategoryList");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
         }
 
 
